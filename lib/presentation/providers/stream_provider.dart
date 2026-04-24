be17081af18e20ba1030e87ccbe7f5c5527d73b3
Jonnathan <jonnathan.valdez@ucuenca.edu.ec>
2026-04-24 16:16:21 -0500

    Add stream provider for users in chat and implement StreamView in StreamProviderScreen

---
+++ lib/presentation/providers/stream_provider.dart
@@ -0,0 +1,15 @@
+import 'package:example/config/config.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'stream_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+Stream<List<String>> usersInChat(UsersInChatRef ref) async* {
+  final names = <String>[];
+  //yield names;
+
+  await for (final name in RandomGenerator.randomNameStream()){
+    names.add(name);
+     yield names;
+  }
+}
